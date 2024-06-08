6714f724d85264fec8159bbfbd991626a8973b77
squach90 <160175686+squach90@users.noreply.github.com>
2024-06-08 11:18:36 +0200

    first commit

---
+++ src/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+import "./assets/VisbyRoundCF-Regular.woff";
+
+import { ContextProvider } from "./functions/context";
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+root.render(
+  <ContextProvider>
+    <App />
+  </ContextProvider>
+);
